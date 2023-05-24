3f94a108778bde7972ddd92acead2bc0d510ea9b
김수연 <suyeon@doubleugames.com>
2023-05-24 11:53:11 +0900

    feat: 썬데이 메이플 공지기능 구현

--- sunday_discord.py
+++ sunday_discord.py
@@ -1,18 +1,45 @@
 import config.botconfig as config
+import webCrowling as web
 import discord
-import asyncio
+import locale
 from discord.ext import commands
 
+# Set the locale to Korean
+locale.setlocale(locale.LC_TIME, 'ko_KR')
+
+# 반복 작업을 위한 패키지
+from discord.ext import tasks
+# 현재 시간을 받아와 구조체에 넣어주는 용도로 사용할 패키지
+import datetime
+# 중복 전송을 방지하기 위해 사용할 패키지
+import time
+
 bot = commands.Bot(command_prefix='!',intents=discord.Intents.all())
+check = True
 
 @bot.event
 async def on_ready():
     print('Done')
     await bot.change_presence(status=discord.Status.online, activity=None)
-    while True:
-        channel = bot.get_channel(config.getChannelId())  # 메시지를 보낼 채널 ID로 바꿔주세요.
-        await channel.send('일정 시간마다 실행되는 작업입니다.')
-        await asyncio.sleep(5)
+        
+@tasks.loop(minutes=1)
+async def every_hour_notice(self):
+    if check :
+        now = datetime.datetime.now().weekday()
+        if now == 3 or now == 4:    # 목, 금에만
+            channel = bot.get_channel(config.getChannelId())  # 메시지를 보낼 채널 ID로 바꿔주세요.
+            await channel.send('Check Sunday')
+            mapleEvents = web.getMapleEventImgUrlList()
+            for url in mapleEvents:
+                check = False
+                await channel.send(url)
+
+@tasks.loop(hours=24)
+async def reset_flag():
+    # 현재 요일 확인하기 (0: 월요일, 1: 화요일, ..., 6: 일요일)
+    weekday = datetime.datetime.now().weekday()
+    if weekday == 0:
+        check = True
 
 @bot.command()
 async def hello(ctx):
--- webCrowling.py
+++ webCrowling.py
@@ -2,11 +2,22 @@ from logging import log
 import requests
 from bs4 import BeautifulSoup
 
-mapleEventUrl = "https://maplestory.nexon.com/News/Event/Ongoing"
-response = requests.get(mapleEventUrl)
-html = response.text
+def getMapleEventImgUrlList():
+    mapleEventUrl = "https://maplestory.nexon.com/News/Event/Ongoing"
+    mainResponse = requests.get(mapleEventUrl)
+    mainHtml = mainResponse.text
 
-soup = BeautifulSoup(html, "html.parser")
-for href in soup.find("div", class_="event_board").find_all("dd", class_="data"):
-    print(href.find("a").text)
-    print(href.find("a")["href"])
+    resultList = list()
+    soup = BeautifulSoup(mainHtml, "html.parser")
+    for href in soup.find("div", class_="event_board").find_all("dd", class_="data"):
+        if href.find("a").text == "썬데이 메이플":
+            targetEventUrl = href.find("a")["href"]
+            # print(href.find("a").text)
+            # print(targetEventUrl)
+            # print("https://maplestory.nexon.com" + targetEventUrl)
+            targetResponse = requests.get("https://maplestory.nexon.com" + targetEventUrl)
+            targetHtml = targetResponse.text
+            targetSoup = BeautifulSoup(targetHtml, "html.parser")
+            resultList.append(targetSoup.find("div", class_="new_board_con").find("img")["src"])
+            break
+    return resultList
